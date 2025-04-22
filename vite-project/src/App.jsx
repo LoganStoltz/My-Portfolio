3e4dcaefc263791b3f17ea9946e5bffbc8d8ca4e
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2025-04-21 22:16:47 -0700

    sticky header working

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -36,8 +36,8 @@ function App() {
     return (
         <div className="App">
             <Home />
+            <Header />
             <main>
-                <Header />
                 <Classes />
                 {/*<Projects />
                 <Resume /> WORK HERE*/}
