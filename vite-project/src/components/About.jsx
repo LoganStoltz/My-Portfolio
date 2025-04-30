75174ab0f29caffdb823c7729e0f5cfdfba4a60a
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2025-04-29 22:15:22 -0700

    .

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -7,6 +7,26 @@ const About = () => {
     return (
         <section id="about">
             <h2 className="section-header">About Me</h2>
+            <div class="about-container">
+                
+                <div class="info">
+                    <div className="icon"></div>
+                    <div className="about-me-text"></div>
+                </div>
+
+                <div className="lang-tools-section">
+                    <div className="lang-tool"></div>
+                    <div className="lang-tool"></div>
+                    <div className="lang-tool"></div>
+                    <div className="lang-tool"></div>
+                    <div className="lang-tool"></div>
+                    <div className="lang-tool"></div>
+                    <div className="lang-tool"></div>
+                    <div className="lang-tool"></div>
+                    <div className="lang-tool"></div>
+                </div>
+            </div> 
+
         </section>
     );
 }
