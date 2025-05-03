c72d5a55993631d632852a923ece5e3a70be1fdc
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2025-05-03 02:34:54 -0700

    Update About.jsx

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -10,28 +10,22 @@ const About = () => {
             <div class="about-container">
                 
                 <div class="info">
-                    <div className="icon"></div>
-                    <div className="about-me-text"></div>
+                    <div className="icon">IMAGE</div>
+                    <div className="about-me-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium ab officiis. Cupiditate minus sit sunt incidunt aspernatur, deleniti dolore ex doloribus cum ipsum porro nesciunt modi saepe tempora quis.</div>
                 </div>
 
                 <div className="lang-tools-section">
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    <div className="lang-tool"></div>
-                    
+                    <div className="lang-tool">Java</div>
+                    <div className="lang-tool">CSS</div>
+                    <div className="lang-tool">HTML</div>
+                    <div className="lang-tool">JavaScript</div>
+                    <div className="lang-tool">React</div>
+                    <div className="lang-tool">SQL</div>
+                    <div className="lang-tool">VSC</div>
+                    <div className="lang-tool">GIT</div>
+                    <div className="lang-tool">Firebase</div>
+                    <div className="lang-tool">Python</div>
+                    <div className="lang-tool">MySQL</div>
                 </div>
             </div> 
 
