406fa75c582be8267bb31663d38e1b9707aec151
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2025-05-04 22:27:09 -0700

    cleaning up

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -7,25 +7,90 @@ const About = () => {
     return (
         <section id="about">
             <h2 className="section-header">About Me</h2>
-            <div class="about-container">
+            <div class="about-me-page-container">
                 
-                <div class="info">
-                    <div className="icon">IMAGE</div>
-                    <div className="about-me-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium ab officiis. Cupiditate minus sit sunt incidunt aspernatur, deleniti dolore ex doloribus cum ipsum porro nesciunt modi saepe tempora quis.</div>
+                
+                <div class="about-me-section">
+                    <div className="about-me-icon">
+                        IMAGE/ICON (placeholder)
+                    </div>
+                    <div className="about-me-text-box">
+                        <div className="about-me-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium ab officiis. Cupiditate minus sit sunt incidunt aspernatur, deleniti dolore ex doloribus cum ipsum porro nesciunt modi saepe tempora quis.
+                        </div>
+                    </div>
                 </div>
 
-                <div className="lang-tools-section">
-                    <div className="lang-tool">Java</div>
-                    <div className="lang-tool">CSS</div>
-                    <div className="lang-tool">HTML</div>
-                    <div className="lang-tool">JavaScript</div>
-                    <div className="lang-tool">React</div>
-                    <div className="lang-tool">SQL</div>
-                    <div className="lang-tool">VSC</div>
-                    <div className="lang-tool">GIT</div>
-                    <div className="lang-tool">Firebase</div>
-                    <div className="lang-tool">Python</div>
-                    <div className="lang-tool">MySQL</div>
+
+
+                <div className="languages-and-tools-section">
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            Java
+                        </div>
+                    </div>
+
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            CSS
+                        </div>
+                    </div>
+
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            HTML
+                        </div>
+                    </div>
+                    
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            JavaScript
+                        </div>
+                    </div>
+
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            React
+                        </div>
+                    </div>
+
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            SQL
+                        </div>
+                    </div>
+
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            VSC
+                        </div>
+                    </div>
+                    
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            GIT
+                        </div>
+                    </div>
+
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            FIREBASE
+                        </div>
+                    </div>
+
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            Python
+                        </div>
+                    </div>
+
+                    <div className="languages-and-tools-boxes">
+                        <div className="languages-and-tools-text">
+                            MySQL
+                        </div>
+                    </div>
+
+
                 </div>
             </div> 
 
